681016af83a688885bab2dcae2eba45fc3300d80
Dov Rine <dov@techimpact.org>
2021-08-18 13:31:49 -0700

    create TodoItem component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import {TodoItem} from './components'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <ul>
+        <TodoItem todo="item 1" />
+        <TodoItem todo="item 2" />
+        <TodoItem todo="item 3" />
+      </ul>
     </div>
   );
 }
