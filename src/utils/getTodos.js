ce45315c6a045f001dcdd2a9635ddb9e1e242b93
Dov Rine <dov@techimpact.org>
2021-08-18 14:25:25 -0700

    refactor getTodos to return an empty array by default

--- src/utils/getTodos.js
+++ src/utils/getTodos.js
@@ -1,8 +1,8 @@
 function getTodos() {
   let arr = []
-  for(let i=0; i<15; i++){
-    arr.push(`item ${i+1}`)
-  }
+  // for(let i=0; i<15; i++){
+  //   arr.push(`item ${i+1}`)
+  // }
   return arr;
 }
 export default getTodos
